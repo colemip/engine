7c5f0ef5730a5e875cf96f2fb9124b79e828c58d
Ian Coleman <colemip@gmail.com>
2013-02-18 00:16:11 -0500

    Up-to-date with coordinates and blitting tutorial, sdltutorials.com

---
+++ sdl/src/CSurface.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef _CSURFACE_H_
+    #define _CSURFACE_H_
+ 
+#include <SDL.h>
+
+class CSurface
+{
+public:
+	CSurface(void);
+	~CSurface(void);
+
+    static SDL_Surface* OnLoad(char* File);
+	static bool OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y);
+};
+
+#endif

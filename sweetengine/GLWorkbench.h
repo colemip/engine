616aa3d16836119cff7393957f6d89562b7e7b7e
Ian Coleman <colemip@gmail.com>
2013-04-09 19:18:48 -0400

    [#17] Adding OpenGL texture example code

--- sweetengine/GLWorkbench.h
+++ sweetengine/GLWorkbench.h
@@ -1,13 +1,29 @@
 #ifndef SWEET_GLWORKBENCH_H
 #define SWEET_GLWORKBENCH_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <glut.h>
+
+/*  Create checkerboard texture  */
+#define checkImageWidth 64
+#define checkImageHeight 64
+
 namespace Sweet 
 {
 	class GLWorkbench
 	{
+	private:
+		static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+		static GLuint texName;
 	public:
 		GLWorkbench();
 		~GLWorkbench();
+		void Init();
+		void makeCheckImage();
+		void display();
+		void reshape(int w, int h);
+		void keyboard (unsigned char key, int x, int y);
 	};
 }
 

a0657fd6de87f7cb48fa5663c8f075cff338c4ee
Ian Coleman <colemip@gmail.com>
2013-04-13 21:18:10 -0400

    [12] Adding CircleCollider class

---
+++ sweetengine/src/CircleCollider.cpp
@@ -0,0 +1,12 @@
+#include "CircleCollider.h"
+
+using namespace Sweet;
+
+CircleCollider::CircleCollider()
+{
+}
+
+
+CircleCollider::~CircleCollider()
+{
+}
---
+++ sweetengine/src/CircleCollider.h
@@ -0,0 +1,16 @@
+#ifndef SWEET_CIRCLECOLLIDER_H
+#define SWEET_CIRCLECOLLIDER_H
+
+#include "Collider.h"
+
+namespace Sweet
+{
+	class CircleCollider : public Collider
+	{
+	public:
+		CircleCollider();
+		~CircleCollider();
+	};
+}
+
+#endif
